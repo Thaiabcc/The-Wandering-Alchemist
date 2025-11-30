7dbcba74bdf8ca5b35f37598a454fe9fd23ecd96
MonMeoww218 <ducthai2182004@gmail.com>
2025-12-01 01:26:43 +0700

    Sketch Forest

--- TheWanderingAlchemist/Assets/_Game/Scripts/Player/PlayerInteraction.cs
+++ TheWanderingAlchemist/Assets/_Game/Scripts/Player/PlayerInteraction.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
@@ -8,12 +8,17 @@ public class PlayerInteraction : MonoBehaviour
     [Header("Setting")]
     [SerializeField] private float checkRadius = 1f;
     [SerializeField] private LayerMask interactLayer;
+    [SerializeField] private float gatherDelay = 0.4f;
 
     private GameControls gameControl;
+    private Animator animator;
+    private PlayerMovement playerMovement;
 
     private void Awake()
     {
         gameControl = new GameControls();
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
     private void OnEnable()
@@ -41,11 +46,30 @@ public class PlayerInteraction : MonoBehaviour
             IInteractable interactable = hit.GetComponent<IInteractable>();
             if(interactable != null)
             {
-                interactable.Interact();
+                StartCoroutine(PerformGather(interactable));
                 return;
             }
         }    
     }
+    // Function wait
+    IEnumerator PerformGather(IInteractable item)
+    {
+        // 1. Locked move
+        if (playerMovement != null) playerMovement.enabled = false;
+
+        // 2. Run ani
+        if (animator != null) animator.SetTrigger("Gather");
+
+        // 3. Time
+        yield return new WaitForSeconds(gatherDelay);
+
+        // 4. Gather
+        item.Interact();
+
+        // 5. Unlocked move
+        if (playerMovement != null) playerMovement.enabled = true;
+    }
+
     // Draw a Circle in Scene
     private void OnDrawGizmosSelected()
     {
