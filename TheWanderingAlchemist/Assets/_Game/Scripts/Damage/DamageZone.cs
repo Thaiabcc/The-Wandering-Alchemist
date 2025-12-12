4b677178d03e7fb6dc3a4466ff6608788d205b7e
MonMeoww218 <ducthai2182004@gmail.com>
2025-12-12 18:48:27 +0700

    feature_basic attack + basic monster

---
+++ TheWanderingAlchemist/Assets/_Game/Scripts/Damage/DamageZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    [Header("Sức mạnh")]
+    [SerializeField] private int damage = 1;
+    [SerializeField] private string targetTag = "Monster"; // Chỉ đánh kẻ địch
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // In ra tên mọi thứ mà kiếm chạm vào để kiểm tra
+        Debug.Log("Kiếm đã chạm vào: " + other.name);
+
+        if (other.CompareTag(targetTag))
+        {
+            Debug.Log("--> Trúng kẻ địch rồi!"); // <--- Nếu hiện dòng này là Tag đúng
+
+            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+            }
+            else
+            {
+                Debug.Log("--> Nhưng nó không có script Máu (EnemyHealth)!");
+            }
+        }
+    }
+}
---
+++ TheWanderingAlchemist/Assets/_Game/Scripts/Damage/PlayerAttack.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerAttack : MonoBehaviour
+{
+    [Header("Cài đặt")]
+    [SerializeField] private GameObject attackPoint; // Kéo cái AttackPoint tàng hình vào đây
+    [SerializeField] private Animator animator;      // Kéo Animator của Player vào
+    [SerializeField] private float attackTime = 0.3f; // Thời gian gây sát thương (chỉnh cho khớp animation)
+    [SerializeField] private float attackCooldown = 0.5f;
+
+    private bool isAttacking = false;
+    private void Awake()
+    {
+        // Tự động tìm Animator trên chính object này
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
+
+    private void Start()
+    {
+        // Tắt vùng sát thương ngay từ đầu cho chắc
+         if (attackPoint != null) attackPoint.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // Chặn không cho spam nút khi đang đánh
+        if (isAttacking) return;
+
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(AttackRoutine());
+        }
+    }
+
+    private IEnumerator AttackRoutine()
+    {
+        isAttacking = true;
+
+        // 1. Chạy Animation (Phần Nhìn)
+        if (animator != null)
+        {
+            animator.SetTrigger("Attack");
+        }
+
+        // 2. Bật vùng sát thương (Phần Dame)
+        // Mẹo: Nếu animation vung tay chậm, bạn có thể thêm yield return new WaitForSeconds(0.1f) ở đây để delay
+        if (attackPoint != null)
+        {
+            attackPoint.SetActive(true);
+        }
+
+        // 3. Giữ vùng sát thương trong thời gian vung gậy
+        yield return new WaitForSeconds(attackTime);
+
+        // 4. Tắt vùng sát thương
+        if (attackPoint != null)
+        {
+            attackPoint.SetActive(false);
+        }
+
+        // 5. Chờ hồi chiêu (để animation kịp quay về Idle)
+        yield return new WaitForSeconds(attackCooldown);
+
+        isAttacking = false;
+    }
+}
